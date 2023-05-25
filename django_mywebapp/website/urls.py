f47572c4ae9d9ef6a27e12cd6952edc86b98dd46
yuyureiri <yui0721w753@gmail.com>
2023-05-25 09:38:15 +0900

    Crate basic pages

--- django_mywebapp/website/urls.py
+++ django_mywebapp/website/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 
-from .views import IndexView
+from .views import IndexView, WorksView, ContactView
 
 urlpatterns = [
     path('', IndexView.as_view()),
+    path('index.html', IndexView.as_view()),
+    path('works.html', WorksView.as_view()),
+    path('contact.html', ContactView.as_view()),
 ]
--- django_mywebapp/website/views.py
+++ django_mywebapp/website/views.py
@@ -6,3 +6,9 @@ from django.views.generic import TemplateView
 
 class IndexView (TemplateView):
     template_name = "index.html"
+
+class WorksView (TemplateView):
+    template_name = "works.html"
+
+class ContactView (TemplateView):
+    template_name = "contact.html"
